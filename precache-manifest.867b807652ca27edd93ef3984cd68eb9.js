cc7697d6a4e49448f9d76b8358c728e24fa2f1d6
Nilton Cosme Felix da Costa Junior <66050129+ncjr1@users.noreply.github.com>
2022-07-17 17:23:48 -0300

    Updates

---
+++ precache-manifest.867b807652ca27edd93ef3984cd68eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "949a487c6ab8ea4f51f8",
+    "url": "/contador-redux/static/css/main.7dff935c.chunk.css"
+  },
+  {
+    "revision": "949a487c6ab8ea4f51f8",
+    "url": "/contador-redux/static/js/main.949a487c.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/contador-redux/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "fb257c85662daf760251",
+    "url": "/contador-redux/static/js/2.fb257c85.chunk.js"
+  },
+  {
+    "revision": "dad890782b1d0470de8b",
+    "url": "/contador-redux/static/js/runtime~main.dad89078.js"
+  },
+  {
+    "revision": "f18a03c4ad53aea951ee01b699643f52",
+    "url": "/contador-redux/index.html"
+  }
+];
